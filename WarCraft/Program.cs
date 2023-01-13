2a278a922e4f908b2f785d83a22bb88e09e9219f
riazonbin <riazonbin@mail.ru>
2023-01-13 11:04:07 +0300

    extracted fight actions to methods

--- WarCraft/Program.cs
+++ WarCraft/Program.cs
@@ -1,9 +1,10 @@
-﻿using Units.ActiveUnits;
+﻿using System.Threading.Tasks;
+using Units.ActiveUnits;
 using Units.BaseUnits;
 
-Footman f1 = new(80, 100, 100, 15, "Alexander");
+Footman f1 = new(80, 100, 15, 15, "Alexander");
 Peasant p1 = new(100, "Bulat");
-Mage m1 = new(80, 100, 8, 100, 500, 8, "Mage");
+Mage m1 = new(80, 100, 8, 100, 500, 12, "Mage");
 Mage m2 = new(80, 100, 10, 100, 100, 4, "UltraMaga");
 Footman f2 = new(80, 100, 10, 15, "Magomed");
 
@@ -18,46 +19,43 @@ Fight(f1, m1);
 
 void Fight(Unit unit1 ,Unit unit2)
 {
-    List<Unit> units = new List<Unit>(){ unit1, unit2 };
+    List<Unit> units = new List<Unit>() { unit1, unit2 };
 
-    var task1 = Task.Run(() =>
+    var task1 = Task.Run((Action)(() =>
     {
-        try
-        {
-            while(units.Count == 2)
-            {
-                unit1.Attack(unit2);
-                Thread.Sleep(((Military)unit1).GetAttackSpeed() * 10);
-
-                if (!unit2.GetStateOfLife())
-                {
-                    units.Remove(unit2);
-                }
-            }
-        }
-        catch { }
-    });
+        UnitAttack(unit1, unit2, units);
+    }));
 
     var task2 = Task.Run(() =>
     {
-        try
-        {
-            while (units.Count == 2)
-            {
-                unit2.Attack(unit1);
-                Thread.Sleep(((Military)unit2).GetAttackSpeed() * 10);
-
-                if (!unit1.GetStateOfLife())
-                {
-                    units.Remove(unit2);
-                }
-            }
-        }
-        catch { }
+        UnitAttack(unit2, unit1, units);
     });
 
+    Task.WaitAll(task1, task2);
+    AttackResult(units, task1, task2);
+}
 
-    if(units.Count == 1)
+static void UnitAttack(Unit attacker, Unit defender, List<Unit> units)
+{
+    try
+    {
+        while (units.Count == 2)
+        {
+            attacker.Attack(defender);
+            Thread.Sleep(((Military)attacker).GetAttackSpeed() * 10);
+
+            if (!defender.GetStateOfLife())
+            {
+                units.Remove(defender);
+            }
+        }
+    }
+    catch { }
+}
+
+static void AttackResult(List<Unit> units, Task task1, Task task2)
+{
+    if (units.Count == 1)
     {
         Console.WriteLine($"Игрок {units[0].Name} победил!");
     }
